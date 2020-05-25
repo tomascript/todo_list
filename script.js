505824e07346c16d48b2570649ead89c7f17c80f
tomascript <tomas.ko@centrum.sk>
2020-05-25 11:52:09 +0200

    add local storage & update media queries

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
-let itemsList; 
+let itemsList, todos; 
 const main = document.querySelector('.main')
 const input = document.querySelector('#form-todo input[type="text"]')
 const deleteAllIcon = document.querySelector('.delete-all i')
+const locStorage = window.localStorage;
+
+todos = JSON.parse(locStorage.getItem('todos'));
+if (!todos) todos = []
+
+const addItemToMarkup = item => {
+    itemsList = document.querySelector('.items-list')
+    itemsList.insertAdjacentHTML('beforeend', 
+    `
+    <li class="row">${item}<span class="icon-span"><i class="fas fa-trash-alt"></i></span></li>
+    `)
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    e.preventDefault();
+    if (todos.length > 0) {
+        for (todo of todos) {
+            addItemToMarkup(todo)
+        }
+    }
+    input.focus()
+});
 
 const clickFunction = (e) => {
 
+    e.preventDefault();
     const items = Array.from(main.children[0].children)
     
     if (items.includes(e.target)) {
         if (e.type === 'click') {
             e.target.classList.toggle('checkout')
         } 
-        e.stopPropagation();
         return;  
      }
 
      if (e.target.nodeName === 'SPAN' || e.target.nodeName === 'I') {
         const item = e.target.parentNode.parentNode
         item.parentNode.removeChild(item)
-        e.stopPropagation();
-        return;
+        todos = JSON.parse(locStorage.getItem('todos'));
+        const updatedList = todos.filter(todo => todo !== item.textContent)
+        locStorage.setItem('todos', JSON.stringify(updatedList))
+        
+        input.focus();
      }
 }
 
-main.addEventListener('click', clickFunction)
+main.addEventListener('click', clickFunction);
 
 deleteAllIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+    locStorage.removeItem('todos');
     itemsList = document.querySelectorAll('.row')
     
     if (itemsList) {
         itemsList.forEach(item => {
             item.parentNode.removeChild(item)
         })
-        e.stopPropagation();
-        return;
     }
 })
 
 document.addEventListener('keypress', (e) => {
    
     if (e.which === 13 && input.value) {
-        itemsList = document.querySelector('.items-list')
-        itemsList.insertAdjacentHTML('beforeend', 
-        `
-        <li class="row">${input.value}<span class="icon-span"><i class="fas fa-trash-alt"></i></span></li>
-        `)
+
+        addItemToMarkup(input.value);
+
+        todos.push(input.value)
+        locStorage.setItem('todos', JSON.stringify(todos))
+
+        console.log(locStorage.todos)
+
         input.value = ''
-        e.stopPropagation();
+        input.focus();
+        
+        e.preventDefault();
         return;
     }
 })
