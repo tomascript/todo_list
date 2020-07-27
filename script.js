6b0dfd0b9d7efa02e774876b3e3d7c4932e6ff9d
tomascript <tomas.ko@centrum.sk>
2020-07-28 00:11:09 +0200

    correct clickFunction for localStorage

--- script.js
+++ script.js
@@ -1,129 +1,121 @@
-let itemsList, todos; 
-const main = document.querySelector('.main')
-const input = document.querySelector('#form-todo input[type="text"]')
-const deleteAllIcon = document.querySelector('.delete-all i')
+let todos, lis;
+const main = document.querySelector('.main');
+const itemsList = main.querySelector('.items-list');
+const input = document.querySelector('#form-todo input[type="text"]');
+const deleteAllIcon = document.querySelector('.delete-all i');
 const locStorage = window.localStorage;
 
 const getTodos = () => {
-    todos = JSON.parse(locStorage.getItem('todos'));
-    if (!todos) todos = []
-}
+  todos = JSON.parse(locStorage.getItem('todos'));
+  if (!todos) todos = [];
+};
 
-const isDuplicate = (val) => {
-    const lis = document.querySelectorAll('.items-list > li')
-   
-    for (li of lis ) {
-        if (val === li.textContent) {
-            return true;
-        }
+const isDuplicate = val => {
+  lis = itemsList.querySelectorAll('li');
+
+  if (lis) {
+    for (li of lis) {
+      if (val === li.textContent) {
+        return true;
+      }
     }
-    return false;    
-}
+  }
+  return false;
+};
 
 const addItemToMarkup = (itemText, isChecked) => {
-    let classToAdd;
-    if (isChecked) {
-        classToAdd = 'row checkout'
-    } else {
-        classToAdd = 'row'
-    }
-    itemsList = document.querySelector('.items-list');
-
-    itemsList.insertAdjacentHTML('beforeend', 
+  let classToAdd;
+  if (isChecked) {
+    classToAdd = 'row checkout';
+  } else {
+    classToAdd = 'row';
+  }
+  itemsList.insertAdjacentHTML(
+    'beforeend',
     `
     <li class="${classToAdd}">${itemText}<span class="icon-span"><i class="fas fa-trash-alt"></i></span></li>
-    `)
-}
+    `
+  );
+};
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', e => {
+  getTodos();
 
-    getTodos();
-    if (todos.length > 0 && todos instanceof Array) {
-        for (todo of todos) {
-            addItemToMarkup(todo.itemText, todo.isChecked)
-        }
+  if (todos.length > 0 && todos instanceof Array) {
+    for (todo of todos) {
+      addItemToMarkup(todo.itemText, todo.isChecked);
     }
-    input.focus()
+  }
+  input.focus();
 });
 
-const clickFunction = (e) => {
-    
-    // e.preventDefault();
-    // e.stopPropagation();
-    
-    const items = Array.from(main.children[0].children)
-    
-    if (items.includes(e.target)) {
-        
-        e.target.classList.toggle('checkout')
-        getTodos();
+const clickFunction = e => {
+  // e.preventDefault();
+  //   e.stopPropagation();
 
-        for (todo of todos) {
-            if (todo.itemText === e.target.textContent) {
-                todo.isChecked = !todo.isChecked;
-                break;
-            }
-        } 
-        locStorage.setItem('todos', JSON.stringify(todos))
-        return;  
+  getTodos();
+  lis = Array.from(itemsList.querySelectorAll('li'));
+
+  if (e.target.nodeName === 'LI' && lis.includes(e.target)) {
+    e.target.classList.toggle('checkout');
+
+    for (todo of todos) {
+      if (todo.itemText === e.target.textContent) {
+        todo.isChecked = !todo.isChecked;
+        break;
+      }
     }
+    locStorage.setItem('todos', JSON.stringify(todos));
+    return;
+  }
 
-     if (e.target.nodeName === 'SPAN' || e.target.nodeName === 'I') {
-        const item = e.target.parentNode.parentNode
-        const updatedList = todos.filter(todo => todo.itemText !== item.textContent)
+  if (e.target.nodeName === 'SPAN' || e.target.nodeName === 'I') {
+    const item = e.target.closest('li');
+    const updatedList = todos.filter(
+      todo => todo.itemText !== item.textContent
+    );
 
-        item.parentNode.removeChild(item)
+    locStorage.setItem('todos', JSON.stringify(updatedList));
+    item.parentNode.removeChild(item);
 
-        locStorage.setItem('todos', JSON.stringify(updatedList))
-        
-        input.focus();
-     }
-}
+    input.focus();
+  }
+};
 
 main.addEventListener('click', clickFunction);
 
-deleteAllIcon.addEventListener('click', (e) => {
+deleteAllIcon.addEventListener('click', e => {
+  // e.preventDefault();
+  // e.stopPropagation();
 
-    // e.preventDefault();
-    // e.stopPropagation();
+  locStorage.removeItem('todos');
+  lis = itemsList.querySelectorAll('li');
 
-    locStorage.removeItem('todos');
-    itemsList = document.querySelectorAll('.row')
-    
-    if (itemsList) {
-        itemsList.forEach(item => {
-            item.parentNode.removeChild(item)
-        })
-    }
-    input.focus();
-})
+  if (lis) {
+    lis.forEach(item => {
+      item.parentNode.removeChild(item);
+    });
+  }
+  input.focus();
+});
 
-document.addEventListener('keypress', (e) => {
+document.addEventListener('keypress', e => {
+  // e.preventDefault();
+  // e.stopPropagation();
 
-    // e.preventDefault();
-    // e.stopPropagation();
+  if (e.which === 13 && input.value) {
     getTodos();
+    if (!isDuplicate(input.value)) {
+      const newItem = {
+        itemText: input.value,
+        isChecked: false
+      };
+      todos.push(newItem);
+      locStorage.setItem('todos', JSON.stringify(todos));
 
-    if (e.which === 13 && input.value ) {
-
-        if (!isDuplicate(input.value)) {
-            
-            const newItem = ({
-                    itemText: input.value, 
-                    isChecked: false
-            })
-            todos.push(newItem)
-            locStorage.setItem('todos', JSON.stringify(todos))
-
-            addItemToMarkup(newItem.itemText, newItem.check);
-
-            input.value = ''
-            input.focus();
-        }
+      addItemToMarkup(newItem.itemText, newItem.isChecked);
     }
-})
-
-
-
-
-
+    input.value = '';
+    input.focus();
+  }
+});
